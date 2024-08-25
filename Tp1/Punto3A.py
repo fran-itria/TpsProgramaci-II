9a6878fb297161405aab8ebcfc2130dcb4123fac
narew97 <nareacaete997@gmail.com>
2024-08-25 20:01:21 -0300

    listo

--- Tp1/Punto3A.py
+++ Tp1/Punto3A.py
@@ -8,3 +8,35 @@
 # iii. El cálculo debe realizarse utilizando algún tipo de bucle (ej: for,
 # while).
 # BONUS: Luego, codificar una función equivalente que utilice recursividad.
+
+
+
+# pide al usuario que ingrese un numero y verifica que el dato ingresado sea un número
+def numero_ingresado ():
+    pedir_numero = input ("Ingrese un número positivo: ")
+    while not pedir_numero.isdigit():
+        print ("El número ingresado no corresponde a un número positivo, por favor intente nuevamente.")
+        pedir_numero = input ("Ingrese un número positivo: ")
+    return int(pedir_numero)
+
+def suma_iterativa(numero):
+    contador = 0
+    for xx in range (1, numero +1):
+        contador += xx
+    return contador
+
+def suma_recursiva(numero):
+    if numero == 1:
+        return 1
+    else:
+        return numero + suma_recursiva(numero -1)
+    
+numero = numero_ingresado()
+
+resultado_iterativo= suma_iterativa(numero)
+
+resultado_recursivo = suma_recursiva(numero)
+
+print (f"La suma de los números del 1 al {numero} usando iteración es: {resultado_iterativo}\n")
+
+print(f"El resultado de la suma de los números de 1 a {numero} usando recursividad es: {resultado_recursivo}\n")

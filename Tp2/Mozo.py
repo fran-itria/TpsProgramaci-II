1d240db0aece2988b2bb9529379ac8d95f7a3174
fran-itria <francoitria01@gmail.com>
2024-09-09 18:56:03 -0300

    Modificacion de comentarios

--- Tp2/Mozo.py
+++ Tp2/Mozo.py
@@ -1,21 +1,30 @@
+# a. El atributo pizzas se inicializa como una lista vacía.
+
+# b. El comando tomarPizzas agrega los objetos de la clase Pizza referenciados por 
+# el parámetro formal pizzas. El mozo puede tomar hasta un máximo de 2 pizzas.
+
+# c. servirPizzas limpia la lista pizzas, haciendo entrega de los pedidos a los clientes.
+
+# d. obtenerEstadoLibre debe retornar True si es que la lista referenciada por el 
+# atributo pizzas tiene una longitud de entre 0 y 1. Así mismo, debe retornar False 
+# si su tamaño es igual a 2.
 class Mozo:
 
     def __init__(self, nombre):
         self.nombre = nombre
-        self.pizzas = []        # El atributo pizzas se inicializa como una lista vacía
+        self.pizzas = []
         
     def establecerNombre(self, nombre):
         self.nombre = nombre
-        
-# El comando tomarPizzas agrega los objetos de la clase Pizza referenciados por 
-# el parámetro formal pizzas. El mozo puede tomar hasta un máximo de 2 pizzas
+    
+    # Punto b 
     def tomarPizzas(self, pizzas):
         if len(self.pizzas) < 2:
             self.pizzas.extend(pizzas[:2])        
         else:
             raise ValueError('El mozo puede tomar hasta 2 pizzas')
-    
-# servirPizzas limpia la lista pizzas, haciendo entrega de los pedidos a los clientes
+
+    # Punto c 
     def servirPizzas(self):
         self.pizzas.clear()
     
@@ -24,10 +33,7 @@ class Mozo:
     
     def obtenerPizzas(self):
         return self.pizzas
-    
-# obtenerEstadoLibre retorna True si es que la lista referenciada
-# por el atributo pizzas tiene una longitud de entre 0 y 1. 
-# Retorna False si su tamaño es igual a 2
 
+    # Punto d
     def obtenerEstadoLibre(self):
         return 0 <= len(self.pizzas) <= 1

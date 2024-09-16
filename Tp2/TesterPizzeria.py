be927139d10326e3f16366c0a3c5e24396cdeeae
fran-itria <francoitria01@gmail.com>
2024-09-16 19:03:57 -0300

    Forma correcta de la clase Tester

--- Tp2/TesterPizzeria.py
+++ Tp2/TesterPizzeria.py
@@ -3,27 +3,29 @@ from Mozo import Mozo
 # from Pizza import Pizza
 
 class TesterPizzeria:
-    def __init__(self):
+    def main(self):
         self.maestro_pizzero = MaestroPizzero("Hugo")
         self.mozo1 = Mozo("Juan")
         self.mozo2 = Mozo("Ignacio")
+        
+        self.maestro_pizzero.obtenerNombre()
+        self.maestro_pizzero.tomarPedido("Hawaiana")
+        self.maestro_pizzero.tomarPedido("Fugazzeta")
+        self.maestro_pizzero.tomarPedido("Pepperoni")
+        self.maestro_pizzero.tomarPedido("Cuatro quesos")
+        self.maestro_pizzero.cocinar()
+        print()
+        primera_tanda = self.maestro_pizzero.entregar()
+        self.mozo1.tomarPizzas(primera_tanda)
+        self.mozo1.servirPizzas()
+        print()
+
+        segunda_tanda = self.maestro_pizzero.entregar()
+        self.mozo2.tomarPizzas(segunda_tanda)
+        self.mozo2.servirPizzas()
 
 
 if __name__ == "__main__":
     testerPizzeria = TesterPizzeria()
-    testerPizzeria.maestro_pizzero.obtenerNombre()
-    testerPizzeria.maestro_pizzero.tomarPedido("Hawaiana")
-    testerPizzeria.maestro_pizzero.tomarPedido("Fugazzeta")
-    testerPizzeria.maestro_pizzero.tomarPedido("Pepperoni")
-    testerPizzeria.maestro_pizzero.tomarPedido("Cuatro quesos")
-    testerPizzeria.maestro_pizzero.cocinar()
-    print()
-    primera_tanda = testerPizzeria.maestro_pizzero.entregar()
-    testerPizzeria.mozo1.tomarPizzas(primera_tanda)
-    testerPizzeria.mozo1.servirPizzas()
-    print()
-
-    segunda_tanda = testerPizzeria.maestro_pizzero.entregar()
-    testerPizzeria.mozo2.tomarPizzas(segunda_tanda)
-    testerPizzeria.mozo2.servirPizzas()
+    testerPizzeria.main()
 

f52af09e896f18b36871f432a6a50df218f3c921
Diego Vazquez <vazquezdiegoda@gmail.com>
2024-10-11 21:51:40 -0300

    punto 6

--- Tp2/TesterPizzeria.py
+++ Tp2/TesterPizzeria.py
@@ -1,13 +1,15 @@
 from MaestroPizzero import MaestroPizzero
 from Mozo import Mozo
+
 # from Pizza import Pizza
+
 
 class TesterPizzeria:
     def main(self):
         self.maestro_pizzero = MaestroPizzero("Hugo")
         self.mozo1 = Mozo("Juan")
         self.mozo2 = Mozo("Ignacio")
-        
+
         self.maestro_pizzero.obtenerNombre()
         self.maestro_pizzero.tomarPedido("Hawaiana")
         self.maestro_pizzero.tomarPedido("Fugazzeta")
@@ -28,4 +30,3 @@ class TesterPizzeria:
 if __name__ == "__main__":
     testerPizzeria = TesterPizzeria()
     testerPizzeria.main()
-
